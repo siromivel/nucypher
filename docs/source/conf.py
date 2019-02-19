76efb0ccd83604fefcc76fc36ae79b06af7e08dd
Levi Morris <siromivel@gmail.com>
2019-02-18 20:05:59 -0700

    wat

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -211,7 +211,7 @@ def setup(app):
     local_source_root = 'https://docs.nucypher.com/en/latest/'
 
     app.add_config_value('recommonmark_config', {
-            'url_resolver': lambda url: local_source_root + '/elmo/' + url,
+            'url_resolver': lambda url: local_source_root + '' + url,
             'auto_toc_tree_section': 'Contents',
             }, True)
     app.add_transform(AutoStructify)
